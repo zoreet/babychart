448ed880a1d730e8a0f399041dc02954d0b50e8e
Vlad Zinculescu <vlad.zinculescu@booking.com>
2017-04-07 18:19:50 +0200

    use BAM naming convention

--- index.php
+++ index.php
@@ -18,37 +18,43 @@
 		</div>
 		<section id="chart" class="panel" data-bind="foreach: days">
 			<div class="day">
-				<div class="day-top sticky">
-					<h2><span data-bind="text: title"></span></h2>
-					<div>Day <span data-bind="text: $root.days().length - $index()"></span></div>
-					<div class="day-head day-row" data-bind="visible: records().length > 0">
-						<div class="day-cell day-cell--time">🕑</div>
-						<div class="day-cell day-cell--pumped">⛽️</div>
-						<div class="day-cell day-cell--breastFed">👩</div>
-						<div class="day-cell day-cell--fed">🍼</div>
-						<div class="day-cell day-cell--formula">🍼F</div>
-						<div class="day-cell day-cell--pipi">💧</div>
-						<div class="day-cell day-cell--caca">💩</div>
+				<div class="day__top sticky">
+					<h2 class="day__subtitle">
+						<span data-bind="text: title"></span>
+						<!-- <span>fed <span data-bind="text: totalFed"></span>ml</span> -->
+					</h2>
+					<div class="day__subtitle">
+						<span>Day <span data-bind="text: $root.days().length - $index()"></span></span>
+						<!-- <span>Daily average <span data-bind="text: $root.avgMilkPerMeal"></span> per 🍼</span> -->
+					</div>
+					<div class="day__head day__row" data-bind="visible: records().length > 0">
+						<div class="day__cell day__cell--time">🕑</div>
+						<div class="day__cell day__cell--pumped">⛽️</div>
+						<div class="day__cell day__cell--breastFed">👩</div>
+						<div class="day__cell day__cell--fed">🍼</div>
+						<div class="day__cell day__cell--formula">🍼F</div>
+						<div class="day__cell day__cell--pipi">💧</div>
+						<div class="day__cell day__cell--caca">💩</div>
 					</div>
 				</div>
-				<div class="day-body" data-bind="foreach: records">
-					<div class="day-row">
-						<div class="day-cell day-cell--time"><input step="1" type="time" data-bind="value: time, event: { change: function() { bbc.saveData(); } }" /></div>
-						<div class="day-cell day-cell--pumped"><input class="number" step="1" type="number" data-bind="value: pumped, event: { change: function() { bbc.saveData(); } }" /></div>
-						<div class="day-cell day-cell--breastFed"><input class="number" step="1" type="number" data-bind="value: breastFed, event: { change: function() { bbc.saveData(); } }" /></div>
-						<div class="day-cell day-cell--fed"><input class="number" step="1" type="number" data-bind="value: fed, event: { change: function() { bbc.saveData(); } }" /></div>
-						<div class="day-cell day-cell--formula"><input class="number" step="1" type="number" data-bind="value: formula, event: { change: function() { bbc.saveData(); } }" /></div>
-						<div class="day-cell day-cell--pipi"><input type="checkbox" class="checkbox" data-bind="checked: pipi, event: { change: function() { bbc.saveData(); } }" /><span>💧<span></div>
-						<div class="day-cell day-cell--caca"><input type="checkbox" class="checkbox" data-bind="checked: caca, event: { change: function() { bbc.saveData(); } }" /><span>💩<span></div>
+				<div class="day__body" data-bind="foreach: records">
+					<div class="day__row">
+						<div class="day__cell day__cell--time"><input step="1" type="time" data-bind="value: time, event: { change: function() { bbc.saveData(); } }" /></div>
+						<div class="day__cell day__cell--pumped"><input class="number" step="1" type="number" data-bind="value: pumped, event: { change: function() { bbc.saveData(); } }" /></div>
+						<div class="day__cell day__cell--breastFed"><input class="number" step="1" type="number" data-bind="value: breastFed, event: { change: function() { bbc.saveData(); } }" /></div>
+						<div class="day__cell day__cell--fed"><input class="number" step="1" type="number" data-bind="value: fed, event: { change: function() { bbc.saveData(); } }" /></div>
+						<div class="day__cell day__cell--formula"><input class="number" step="1" type="number" data-bind="value: formula, event: { change: function() { bbc.saveData(); } }" /></div>
+						<div class="day__cell day__cell--pipi"><input type="checkbox" class="checkbox" data-bind="checked: pipi, event: { change: function() { bbc.saveData(); } }" /><span>💧<span></div>
+						<div class="day__cell day__cell--caca"><input type="checkbox" class="checkbox" data-bind="checked: caca, event: { change: function() { bbc.saveData(); } }" /><span>💩<span></div>
 					</div>
 				</div>
-				<div class="day-foot sticky">
+				<div class="day__foot sticky">
 					<button type="button" data-bind="click: addRecord">Add Record</button>
-					<div class="day-stats">
-						<div class="day-stats--row">
+					<div class="day__stats">
+						<div class="day__stats__row">
 							💪<input type="number" step="1" data-bind="value: weight" /> grams
 						</div>
-						<div class="day-stats--row">
+						<div class="day__stats__row">
 							🍼<input type="number" step="1" data-bind="value: bottlesPerDay" /> per day
 						</div>
 					</div>
